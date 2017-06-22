a3ff3013bf10539b95c81ec3b49015bc68af0dd3
vveicc <vveicc@163.com>
2017-06-22 23:28:30 +0800

    Fix process animation

--- MarkdownCataloguer/MainViewController.swift
+++ MarkdownCataloguer/MainViewController.swift
@@ -12,13 +12,14 @@ class MainViewController: NSViewController {
     
     @IBOutlet weak var dragView: MCDragView!
     @IBOutlet weak var addFileButton: MCButton!
+    @IBOutlet weak var msgTextField: NSTextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         dragView.receiveFile = { filePath in
-            self.handleAndSaveFile(filePath: filePath)
+            self.processAnimating(filePath: filePath)
         }
     }
     
@@ -38,43 +39,47 @@ class MainViewController: NSViewController {
             if response == NSModalResponseOK {
                 let path = panel.urls.first!.path
                 print("OpenPath: \(path)")
-                self.handleAndSaveFile(filePath: path)
+                self.processAnimating(filePath: path)
             }
         }
     }
     
-    func handleAndSaveFile(filePath: String) {
+    func processAnimating(filePath: String) {
         addFileButton.isEnabled = false
         Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true) { (timer: Timer) in
             self.dragView.progress += 2
             if self.dragView.progress == 100 {
                 timer.invalidate()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.06, execute: { 
+                    self.handleAndSaveFile(filePath: filePath)
+                    self.dragView.progress = 0
+                })
             }
         }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.05) {
-            guard let content = MCHelper.shared.handleMarkdownFile(filePath: filePath) else {
-                return
-            }
-            print("content: \n\(content)")
-            let panel = NSSavePanel()
-            panel.canCreateDirectories = true
-            panel.canSelectHiddenExtension = false
-            panel.isExtensionHidden = false
-            panel.allowedFileTypes = ["md"]
-            panel.nameFieldStringValue = filePath.components(separatedBy: "/").last!
-            panel.begin { (response: NSModalResponse) in
-                if response == NSModalResponseOK {
-                    let path = panel.url!.path
-                    print("SavePath: \(path)")
-                    do {
-                        try content.write(toFile: path, atomically: true, encoding: .utf8)
-                    } catch {
-                        print("保存文件失败！！！")
-                    }
+    }
+    
+    func handleAndSaveFile(filePath: String) {
+        guard let content = MCHelper.shared.handleMarkdownFile(filePath: filePath) else {
+            return
+        }
+        print("content: \n\(content)")
+        let panel = NSSavePanel()
+        panel.canCreateDirectories = true
+        panel.canSelectHiddenExtension = false
+        panel.isExtensionHidden = false
+        panel.allowedFileTypes = ["md"]
+        panel.nameFieldStringValue = filePath.components(separatedBy: "/").last!
+        panel.begin { (response: NSModalResponse) in
+            if response == NSModalResponseOK {
+                let path = panel.url!.path
+                print("SavePath: \(path)")
+                do {
+                    try content.write(toFile: path, atomically: true, encoding: .utf8)
+                } catch {
+                    print("保存文件失败！！！")
                 }
-                self.addFileButton.isEnabled = true
             }
-            self.dragView.progress = 0
+            self.addFileButton.isEnabled = true
         }
     }
 
